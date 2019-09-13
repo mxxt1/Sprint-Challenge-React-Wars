75907a239ab89e7b780131c5033c9d0ec492e9bf
mxxt1 <matt@dustcloudlabs.com>
2019-09-13 11:59:04 -0400

    created component structure

---
+++ starwars/src/components/CharCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+
+
+export default function CharCard(props){
+    return(
+
+
+
+
+
+
+    );
+
+
+
+
+
+
+
+}
---
+++ starwars/src/components/CharContainer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from "styled-components";
+import CharCard from "./CharCard"
+import axios from 'axios';
+
+
+
+export default function CharContainer(){
+
+const [starChars, setStarChars] = useState([]);
+
+
+useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
+    .then((response) => {
+      console.log(response);
+      console.log(response.data.results);
+      setStarChars(response.data.results);
+    })
+    .catch(error => {
+      console.log(`Error: `,error);
+    })
+  },[]);
+
+  return(
+      <div>
+      {starChars.map(character => {
+          return(
+              <CharCard 
+              key={character.name}
+              name={character.name}
+              height = {character.height}
+              weight = {character.mass}
+              birthYear = {character.birth_year}
+              gender = {character.gender}
+              eyeColor = {character.eye_color}
+              skinColor = {character.skin_color}
+              hairColor = {character.hair_color}
+              />
+          )
+      })}
+      
+      
+      
+      </div>  
+
+
+
+
+
+
+
+  )
+
+
+
+
+
+
+
+
+
+}
